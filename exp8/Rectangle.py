6d2ce5eb4e07600bdf7f74ef8ea67915e5a9c261
rahulrahul62023bcs28-alt <rahulrahul62023_bcs28@mepcoeng.ac.in>
2026-04-10 11:53:29 +0530

    Create Rectangle.py

---
+++ exp8/Rectangle.py
@@ -0,0 +1,9 @@
+class Rectangle:
+   def __init__(self,length,breadth):
+      self.length=length
+      self.breadth=breadth
+   def method(self):
+      return ((self.length)*(self.breadth))
+rec=Rectangle(10,5)
+a=rec.method()
+print(a)
