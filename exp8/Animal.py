19f0cc41827efd654a1969a028f94667cf71ed62
rahulrahul62023bcs28-alt <rahulrahul62023_bcs28@mepcoeng.ac.in>
2026-04-10 11:54:34 +0530

    Create Animal.py

---
+++ exp8/Animal.py
@@ -0,0 +1,11 @@
+class Animal:
+   def speak(self):
+      print("animal speaks")
+class Cat(Animal):
+   def speak(self):
+      print("cat tweets")
+
+animal=Animal()
+animal.speak()
+cat=Cat()
+cat.speak()
