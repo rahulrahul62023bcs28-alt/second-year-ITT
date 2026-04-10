07e509ec938ddd2267a4edf5483d7ea536469423
rahulrahul62023bcs28-alt <rahulrahul62023_bcs28@mepcoeng.ac.in>
2026-04-10 11:41:38 +0530

    Add Intersection functionality for two sets
    Implement intersection of two sets and modify them.

---
+++ exp4/Intersection.py
@@ -0,0 +1,8 @@
+HI = {101, 102, 105, 107, 110}
+say = {102, 103, 107, 108, 110}
+both_days = HI.intersection(say)
+print("BOTH days:",both_days)
+print(HI.discard(101))
+print(HI)
+sum=say.add("RAHUL")
+print(say)
